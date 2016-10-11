302ab9d1b48b49e47f9ed1d0ded43a1473291c39
usiege <2275572017@qq.com>
2016-10-11 14:18:03 +0800

    三亚项目合并前

--- SanyaDemo/app/src/main/java/com/xianfeng/sanyademo/DetialActivity.java
+++ SanyaDemo/app/src/main/java/com/xianfeng/sanyademo/DetialActivity.java
@@ -4,8 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class DetialActivity extends AppCompatActivity {
+
+    EditText    username,address,number,gasAmount;
+    TextView    moneyView;
+    Button      submit,facture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,23 +20,34 @@ public class DetialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detial);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        username = (EditText) findViewById(R.id.accountEdittext);
+        address = (EditText) findViewById(R.id.pwdEdittext);
+        number = (EditText) findViewById(R.id.ntext);
+        gasAmount = (EditText) findViewById(R.id.atext);
+
+        moneyView = (TextView) findViewById(R.id.mvalue);
+
+        submit = (Button) findViewById(R.id.read);
+        facture = (Button) findViewById(R.id.write);
+
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
-//        menu.findItem(R.id.menu_scan).setVisible(true);
-//        menu.findItem(R.id.menu_refresh).setActionView(null);
+        menu.findItem(R.id.clear).setVisible(true);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.menu_scan:
-//
-//                break;
-//        }
+        switch (item.getItemId()) {
+            case R.id.clear:
+
+                break;
+        }
         return true;
     }
 
